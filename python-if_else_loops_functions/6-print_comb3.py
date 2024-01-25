87ecda77f12262c719fdadaf62dc7ba26bae666f
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-01-25 09:48:12 +0100

    Python - if/else, loops, functions

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,3 @@ for i in range(0, 10):
             print("{}".format(str(i)+str(j)))
         if i != j and i < j and (str(i) + str(j)) != "89":
             print("{},".format(str(i) + str(j)), end=" ")
-
