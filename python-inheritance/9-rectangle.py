b3ebb9e2762206fc8695ea546b6d5cd394997c11
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-02-11 19:36:09 +0100

    class Rectangle that inherits from BaseGeometry (7-base_geometry.py). (task based on 8-rectangle.py)
    Instantiation with width and height: def __init__(self, width, height)::
    width and height must be private. No getter or setter
    width and height must be positive integers validated by integer_validator
    the area() method must be implemented
    print() should print, and str() should return, the following rectangle description: [Rectangle] <width>/<height>

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -5,6 +5,8 @@ module documentation
 
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """
     function documentation
@@ -17,6 +19,8 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def __str__(self):
-        return f"[{str(self.__class__.__name__)}] {str(self.__width)}/{str(self.__height)}"
+        return f"[{str(self.__class__.__name__)}] /
+        {str(self.__width)}/{str(self.__height)}"
+
     def area(self):
         return self.__width * self.__height
