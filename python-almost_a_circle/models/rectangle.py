384cc91f4fce40b46e6dababe7902f3cb2e240da
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-02-15 17:29:44 +0100

    Write the class Rectangle that inherits from Base:
    In the file models/rectangle.py
    Class Rectangle inherits from Base
    Private instance attributes, each with its own public getter and setter:
    __width -> width
    __height -> height
    __x -> x
    __y -> y
    Class constructor: def __init__(self, width, height, x=0, y=0, id=None):
    Call the super class with id - this super call with use the logic of the __init__ of the Base class
    Assign each argument width, height, x and y to the right attribute
    Why private attributes with getter/setter? Why not directly public attribute?
    
    Because we want to protect attributes of our class. With a setter, you are able to validate what a developer is trying to assign to a variable. So after, in your class you can “trust” these attributes.

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module documentation
+"""
+from models.base import Base
+
+class Rectangle(Base):
+    """
+    function documentation
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
