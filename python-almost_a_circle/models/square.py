d9939f44560dd0db60718690212dce95738555be
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-02-18 16:46:38 +0100

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,4 +10,11 @@ class Square(Rectangle):
     class Square
     """
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialize the square"""
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """string repsentation  of the square"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+    
+    
