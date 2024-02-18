85ce3f10779f227d0b932431841a6861b46cfab7
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-02-19 00:33:56 +0100

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
 module documentation
-
-
 """
+import json
+
 class Base:
     """
     function documentation
@@ -16,3 +16,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
