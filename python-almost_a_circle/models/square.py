c235ae8be6959b85963b925c205cb3aecc34d690
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-02-18 17:06:25 +0100

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -16,3 +16,36 @@ class Square(Rectangle):
     def __str__(self):
         """string repsentation  of the square"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+    
+    @property
+    def size(self):
+        """Getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = self.height = value
+        
+    def __update(self, id=None, size=None, x=None, y=None):
+        """ updates """
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """Updates keyword args."""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+    
