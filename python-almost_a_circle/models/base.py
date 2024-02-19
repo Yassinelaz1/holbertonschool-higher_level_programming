55a2a6701ece1e3dca502e8cb0fa5ddeb8635891
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-02-19 02:28:43 +0100

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,9 +17,40 @@ class Base:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None:
+            with open(f"{cls.__name__}.json", "w") as f:
+                json.dump([], f)
+        else:
+            objs = list(i.to_dictionary() for i in list_objs)
+            with open(f"{cls.__name__}.json", "w") as f:
+                f.write(cls.to_json_string(objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None:
+            return []
+        else:
+            x = list(json.loads(json_string))
+            return x
+
+    @classmethod
+    def create(cls, **dictionary):
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls is Rectangle:
+            uptd = Rectangle(1, 1)
+        elif cls is Square:
+            uptd = Square(1)
+        else:
+            uptd = None
+        uptd.update(**dictionary)
+        return uptd
