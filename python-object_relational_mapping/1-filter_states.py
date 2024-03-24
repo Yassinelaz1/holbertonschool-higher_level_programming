d7adb245a3e3f65f9491e583584348271b4dc499
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-03-24 23:04:33 +0100

    2 and 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+ script that lists all states with aname starting with N
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+    )
+    c = db.cursor()
+    c.execute("SELECT * FROM states ORDER BY states.id")
+    [print(state) for state in cur.fetchall() if state[1].startswith("N")]
